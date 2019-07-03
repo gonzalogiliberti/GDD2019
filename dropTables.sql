31f94b34637a6e527fde1abd909306b9dc839f37
Martin Gauna <martinj.gauna@gmail.com>
2019-07-03 10:48:07 -0300

    wip

--- dropTables.sql
+++ dropTables.sql
@@ -1,5 +1,40 @@
+/*** Drop Cliente ***/
+
 drop table dbo.Reserva
 drop table dbo.ClienteXRecorrido
 drop table [dbo].[Compra]
 drop table [dbo].[Reserva]
 drop table dbo.Cliente
+
+
+
+/****** Drop Todas *******/
+
+drop table Baja
+drop table TipoBaja
+drop table Cabina
+drop table Viaje
+drop table Reserva
+drop table ClienteXRecorrido
+drop table Compra
+drop table Reserva
+drop table Cliente
+drop table MedioPago
+drop table TarjetaCredito
+drop table 
+drop table 
+drop table 
+drop table 
+drop table 
+drop table 
+drop table 
+drop table 
+drop table 
+drop table 
+drop table 
+drop table 
+drop table 
+drop table 
+drop table 
+drop table 
+drop table 
